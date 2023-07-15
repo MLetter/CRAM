2c7b7f519a97e660e3d1ffc75705ca47971c756d
MLetter <139174162+MLetter@users.noreply.github.com>
2023-07-15 11:48:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import math
 import numpy as np
 import math
+
+
+
+@staticmethod
+def load_dataset(filename = 'data1.csv'):
+    A = np.loadtxt(filename,dtype = str, delimiter=",")
+    X = A[1:,1:]
+    X = X.T
+    Y = A[1:,0]
